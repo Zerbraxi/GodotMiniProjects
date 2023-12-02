2aa5dbc9dcc55a69702de7cf3b9a2ebd6de303c1
Zerbraxi <12447950+Zerbraxi@users.noreply.github.com>
2023-12-02 00:19:15 -0500

    Finished Balloon Popper
    Completed Balloon Popper Mini Project
    Also added a click and pop sound effect to give better feedback that wasn't part of tutorial

--- MiniProjects/Balloon Popper/Balloon Manager.gd
+++ MiniProjects/Balloon Popper/Balloon Manager.gd
@@ -1,8 +1,10 @@
 extends Node3D
 
 var score : int = 0
+@export var score_text : Label
 
 func increase_score (amount):
 	score += amount
-	print"(5")
+	score_text.text = str("Score: ",score)
+	print(score)
 	
--- MiniProjects/Balloon Popper/Balloon.gd
+++ MiniProjects/Balloon Popper/Balloon.gd
@@ -1,18 +1,21 @@
 extends Area3D
 
-var clicks_to_pop : int = 3
-var size_increase : float = 0.2
-var score_to_give : int = 1
-
+@export var clicks_to_pop : int = 5
+@export var size_increase : float = 0.2
+@export var score_to_give : int = 1
+@onready var balloon_pop_sound = get_node("/root/Main/BalloonPop")
+@onready var balloon_click_sound = get_node("/root/Main/BalloonClick")
 
 func _on_input_event(camera, event, position, normal, shape_idx):
 	
 	# check if mouse button click
 	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
+		balloon_click_sound.play()
 		scale += Vector3.ONE * size_increase
 		clicks_to_pop -= 1
 		
 		if clicks_to_pop == 0:
-			get_node("/root/Main:").increase
+			get_node("/root/Main:").increase_score(score_to_give)
+			balloon_pop_sound.play()
 			queue_free()
-dd
+
